c852799c7e58ccb7892469ffcd0d6ea48014135a
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-14 16:24:27 -0400

    use semantic ui instead of sass

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,10 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
-  JQuery: 'jquery',
+  jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
+
 module.exports = environment
