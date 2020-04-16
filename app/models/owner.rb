63be914e65ab3c6ae9f728060d69463df1ea4962
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-16 12:19:10 -0400

    add association owner-projects

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,8 @@
 require 'uri'
 
 class Owner < ApplicationRecord
+  has_many :projects
+
   NAME_REGEX = /\A[A-Z][a-z]{2,}\s[A-Z][a-z]{2,}\z/
 
   validates :name, format: { with: NAME_REGEX }
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,15 @@
 require 'uri'
 
 class Project < ApplicationRecord
+  belongs_to :owner
+
   validates :title, length: { minimum: 10, maximum: 150 },
     uniqueness: { case_sensitive: false }
+
   validates :description, length: { minimum: 50, maximum: 300 }
-  validates :site, format: { with: URI.regexp }
-  validates :repo, format: { with: URI.regexp }
+  validates :site, :repo, format: { with: URI.regexp }
+
   scope :active_projects, -> { where(active: true) }
+
   has_one_attached :demo_image
 end
