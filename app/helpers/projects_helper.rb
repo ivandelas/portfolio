574450cf3786c7b9b9063f2d8ef10496186dd83d
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-17 12:39:26 -0400

    add message partial when projects is empty

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def render_projects(projects)
+    if projects.any?
+      render partial: 'projects/project', collection: projects
+    else
+      render partial: 'projects/message'
+    end
+  end
 end
